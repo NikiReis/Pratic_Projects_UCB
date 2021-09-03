df72b1d5c2714aaf28a46118b0e02d87725def28
Linek Reis <linekerreis12@hotmail.com>
2021-09-03 18:14:26 -0300

    Update PROJETO_PRÁTICO_python.py

--- PROJETO_PRÁTICO_python.py
+++ PROJETO_PRÁTICO_python.py
@@ -1,11 +1,15 @@
 from random import randint
 import os
-print("Salumar Airlines")
-print("-"*21)
 
+totalvalue = 0
 count = 0
 classification = []
 armchairs = []
+
+while True:
+    print("Salumar Airlines")
+    print("-"*21)
+
 
 destiny = str(input("Type the name of the destination: "))
 
@@ -17,14 +21,29 @@ while chairs <90 or chairs >200:
 for i in range (0,chairs):
         count += 1
         armchairs.append(count)
+price = int(input("Type the value of the passage: "))
 for i in range (0,chairs):
         classification.append("D")
 os.system('cls' if os.name =='nt' else 'clear')
 print("Options")
 print("-"*7)
-option = int(input("[1] - Diponible chairs: "))
+print("[1] - Diponible chairs\n[2] Reserved or Confirmed boughts\n[3] Total value of the bought \n[4] Cancel a reservation")
+option = int(input("Type the desired option: "))       
 if option == 1:
     for i in range (0,chairs):
             if len(armchairs) <= chairs and classification[i] == "D":
                 print(f"{classification[i]}",end='')
                 print(f"{armchairs[i]:<10}\t")
+elif option == 2:
+    for i in range (0,chairs):
+        if len(armchairs) <= chairs and classification[i] == "R":
+            if classification[i] == "R":
+                print(f"{classification[i]}",end='')
+                print(f"{armchairs[i]:<5}\t")
+elif option == 3:
+    os.system("cls" if os.name == 'nt' else 'clear')
+    print(totalvalue)
+
+endprogram = str(input("You want to buy another passage ?\nType any buttom to confirm or type 'No' to finish the program")).upper()
+if endprogram == 'NO':
+    break
