c206b44490df14c09880fbfa9c27b4af3c56f69c
Linek Reis <linekerreis12@hotmail.com>
2022-04-20 17:42:32 -0300

    Update Main.py

--- Pratic_Project 2 - Pyhton/Version 1.0/Main.py
+++ Pratic_Project 2 - Pyhton/Version 1.0/Main.py
@@ -2,5 +2,4 @@ from InfoViagem import *
 from Interface import *
 
 
-
-menu()
+interacao() ## responsável por chamar a função principal do arquivo 'Interface.py', função está que inicializa todo o sistema
