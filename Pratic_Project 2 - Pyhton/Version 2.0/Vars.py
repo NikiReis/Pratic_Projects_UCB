5e904293d75c0e7479d456a76b60add7198471da
Linek Reis <linekerreis12@hotmail.com>
2022-05-07 14:53:51 -0700

    Update Vars.py

--- Pratic_Project 2 - Pyhton/Version 2.0/Vars.py
+++ Pratic_Project 2 - Pyhton/Version 2.0/Vars.py
@@ -4,6 +4,43 @@ armschairs = random.randint(90,200)
 tripvalue = random.randint(250,2000)
 
 chair_data = list()
+disponible = armschairs
+reserved_chairs = 0
+purchase_chairs = 0
+purchase_value = 0
+reserve_value = 0
+
+def display():
+	print("[1] Mostrar Poltronas Disponíveis/Reservadas/Compradas")
+	print("[2] Realizar uma Reserva")
+	print("[3] Comprar uma passagem")
+	print("[4] Cancelar uma Reserva/Compra")
+	print("[5] Sair do Sistema")
+
+def incremet_purchase():
+
+        disponible -= 1
+        purchase_value += tripvalue
+        purchase_chairs += 1
+
+def decrement_purchase():
+
+    purchase_value -= tripvalue
+    purchase_chairs -= 1
+    disponible += 1
+
+def increment_reserve(): 
+
+    reserve_value += tripvalue
+    reserved_chairs += 1
+    disponible -= 1
+
+def decrement_reserve():
+    reserve_value -= tripvalue
+    if reserve_value < 0:
+        reserve_value = 0
+    reserved_chairs -= 1
+    disponible += 1
 
 for x in range(1,armschairs):
     chair_data.append(['D','0'])
@@ -23,5 +60,6 @@ def trip_informations():
             print("Plese, try again!\n")
         else:
             break
-
 trip_informations()
+
+
