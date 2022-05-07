91eb2ee0d85fc19b53fb7b1cb39436f6b9333791
Linek Reis <linekerreis12@hotmail.com>
2022-05-07 16:56:25 -0700

    Update Vars.py

--- Pratic_Project 2 - Pyhton/Version 2.0/Vars.py
+++ Pratic_Project 2 - Pyhton/Version 2.0/Vars.py
@@ -3,6 +3,7 @@ import random
 armschairs = random.randint(90,200)
 tripvalue = random.randint(250,2000)
 
+id = 0
 chair_data = list()
 disponible = armschairs
 reserved_chairs = 0
@@ -18,24 +19,37 @@ def display():
 	print("[5] Sair do Sistema")
 
 def incremet_purchase():
+        global disponible
+        global purchase_value
+        global purchase_chairs
 
         disponible -= 1
         purchase_value += tripvalue
         purchase_chairs += 1
 
 def decrement_purchase():
+    global disponible
+    global purchase_value
+    global purchase_chairs
 
     purchase_value -= tripvalue
     purchase_chairs -= 1
     disponible += 1
 
 def increment_reserve(): 
+    global reserved_chairs
+    global reserve_value
+    global disponible
 
     reserve_value += tripvalue
     reserved_chairs += 1
     disponible -= 1
 
 def decrement_reserve():
+    global reserved_chairs
+    global reserve_value
+    global disponible
+
     reserve_value -= tripvalue
     if reserve_value < 0:
         reserve_value = 0
