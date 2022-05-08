17073b8b9350203283e81c3e7b6d27c029c8a512
Linek Reis <linekerreis12@hotmail.com>
2022-05-08 15:21:16 -0700

    Update Scripts.py

--- Pratic_Project 2 - Pyhton/Version 2.0/Scripts.py
+++ Pratic_Project 2 - Pyhton/Version 2.0/Scripts.py
@@ -1,12 +1,11 @@
 from Vars import*
 from time import sleep
-import os
 
-while True:      
-    os.system("cls" if os.name=='nt' else "clear")
-    display()
-    def system(userinput):
-        
+while True:     
+    def system(userinput,id=0):
+        global new_id
+        id = new_id
+
         match userinput:
             case 1:
 
@@ -18,12 +17,12 @@ while True:
                             print(f"\033[1;32m{[x][0]}\033[m",end=' ')
                         else:
                             print(f"\033[1;31m{[x][0]}\033m",end=' ')
-                sleep(2)
                 showarmchair()
 
             case 2:
+
                 def do_reserve():
-                    global id
+                    nonlocal id
                     chair_number = int(input("Type the armchair that you want reserve: "))
 
                     if (chair_data[chair_number][0] == "D"):
@@ -37,6 +36,7 @@ while True:
                     elif ((chair_data[chair_number][0] == "C") or (chair_data[chair_number][0] == "R")):
                         print("Cannot reserve the armchair, it's already selected!")
                         sleep(3)
+
                     else:
                         print("Armchair not found!")
                         sleep(3)
@@ -44,8 +44,9 @@ while True:
                 do_reserve()
 
             case 3:
+
                 def buy_trip():
-                    global id
+                    nonlocal id
                     chair_number = int(input("Type the armchair number that you want to buy: "))
 
                     if (chair_data[chair_number][0] == "R"):
@@ -67,7 +68,7 @@ while True:
 
                     elif (chair_data[chair_number][0] == "C"):
                         print("Compra não realizada!")
-                        print("Esta poltrona já foi comprada/reservada para a passagem de outra pessoa!")
+                        print("Esta poltrona já foi comprada para a passagem de outra pessoa!")
                         sleep(3)
                     else:
                         print("Poltrona não encontrada")
@@ -77,12 +78,14 @@ while True:
                 buy_trip()   
 
             case 4:
+
                 def cancel_trip():
-                    global id
+                    nonlocal id
                     chair_number = int(input("Digite o numero da poltrona que deseja cancelar: "))
 
                     if (chair_data[chair_number][0] == "R"):
                         ID = int(input("Digite o id da passagem: "))
+
                         if (str(ID) == chair_data[chair_number][1]):
                             print("Passagem cancelada")
                             chair_data[chair_number][0] = "D"
@@ -90,6 +93,7 @@ while True:
                             decrement_reserve()
                             id -= 1 
                             sleep(3)
+
                         else:
                             print("ID informado não condiz com ID da poltrona!\n")
                             print("Somente o propietário pode cancelar a reserva")
@@ -103,6 +107,7 @@ while True:
                             chair_data[chair_number][1] = "0"
                             decrement_purchase()
                             id -= 1 
+
                         else:
                             print("ID informado não condiz com ID da poltrona!\n")
                             print("Somente o propietário pode cancelar a compra")
@@ -111,21 +116,30 @@ while True:
                     elif (chair_data[chair_number][0] == "D"):
                         print("Não é possível cancelar uma passagem que não tenha sido comprada / reservada")
                         sleep(3)
+
                     else:
                         print("Poltrona não encontrada")
                         sleep(3)
                 cancel_trip()
+            case out_of_range:
+                print("Valor não indentificado!\nRetornando para a tela inicial\nPor favor escolha apenas os valores mostrados na tela")
+                sleep(5)
+
+        new_id = id
+        return id
+
     while True:
+        clear()
+        display()
         try:
             answer = int(input("\nType the desired option: "))
-        except ValueError or TypeError:
-            print("Valor não identificado!\n")
+        except ValueError:
+            print("Valor não identificado!")
             print("Por favor digite um valor válido")
+            sleep(2)
         else:
             break
     if (answer == 5):
         break
     else:
         system(answer)
- 
-    
