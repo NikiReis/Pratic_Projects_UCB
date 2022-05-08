36be76fa1e288bd1dfa663b5bba90dd2f06cc794
Linek Reis <linekerreis12@hotmail.com>
2022-05-07 19:33:30 -0700

    Update Scripts.py

--- Pratic_Project 2 - Pyhton/Version 2.0/Scripts.py
+++ Pratic_Project 2 - Pyhton/Version 2.0/Scripts.py
@@ -9,12 +9,12 @@ while True:
 
                 def showarmchair():
                     for x in range(0,len(chair_data)):
-                        if chair_data[x][0] == "D":
-                            print(f"\033[1;32m{x}\033[m",end=' ')
-                        elif chair_data[x][0] == "C":
-                            print(f"\033[1;36{x}\033[m",end=' ')
+                        if chair_data[x][0] == "C":
+                            print(f"\033[1;36m{[x][0]}\033[m",end=' ')
+                        elif chair_data[x][0] == "D":
+                            print(f"\033[1;32m{[x][0]}\033[m",end=' ')
                         else:
-                            print(f"\033[1;31m{x}\033[m",end=' ')
+                            print(f"\033[1;31m{[x][0]}\033m",end=' ')
                 showarmchair()
 
             case 2:
@@ -22,15 +22,15 @@ while True:
                     global id
                     chair_number = int(input("Type the armchair that you want reserve: "))
 
-                    if chair_data[chair_number][0] == 'D':
+                    if chair_data[chair_number][0] == "D":
                         id += 1
-                        chair_data[chair_number][0] = 'R'
+                        chair_data[chair_number][0] = "R"
                         chair_data[chair_number][1] = str(id)
                         increment_reserve()
                         print(f"ATTENTION, YOU ONLY HAVE 10 SECONDS TO NOTE YOUR TRIP'S ID. ID: {id}")
                         sleep(10)
 
-                    elif chair_data[chair_number][0] == 'C' or chair_data[chair_number][0] == 'R':
+                    elif chair_data[chair_number][0] == "C" or chair_data[chair_number][0] == "R":
                         print("Cannot reserve the armchair, it's already selected!")
                     else:
                         print("Archair not found!")
@@ -42,23 +42,23 @@ while True:
                     global id
                     chair_number = int(input("Type the armchair number that you want to buy: "))
 
-                    if chair_data[chair_number][0] == 'R':
+                    if chair_data[chair_number][0] == "R":
                         print("Reserva Encontrada")
                         ID = int(input("Digite o id da reserva: "))
                         if str(ID) == chair_data[chair_number][1]:
-                            chair_data[chair_number][0] = 'C'
+                            chair_data[chair_number][0] = "C"
                             print("Passagem comprada")
 
-                    elif chair_data[chair_number][0] == 'D':
+                    elif chair_data[chair_number][0] == "D":
                         id += 1
-                        chair_data[chair_number][0] = 'C'
+                        chair_data[chair_number][0] = "C"
                         chair_data[chair_number][1] = str(id)
                         incremet_purchase()
                         print("Passagem comprada")
                         print(f"ATENÇÃO, VOCÊ TEM 10 SEGUNDOS PARA ANOTAR O ID SUA RESEVA. ID: {id}")
                         sleep(10)
 
-                    elif chair_data[chair_number][0] == 'C':
+                    elif chair_data[chair_number][0] == "C":
                         print("Compra não realizada!")
                         print("Esta poltrona já foi comprada/reservada para a passagem de outra pessoa!")
                     else:
@@ -72,31 +72,31 @@ while True:
                     global id
                     chair_number = int(input("Digite o numero da poltrona que deseja cancelar: "))
 
-                    if chair_data[chair_number][0] == 'R':
+                    if chair_data[chair_number][0] == "R":
                         ID = int(input("Digite o id da passagem: "))
                         if str(ID) == chair_data[chair_number][1]:
                             print("Passagem cancelada")
-                            chair_data[chair_number][0] = 'D'
-                            chair_data[chair_number][1] = '0'
+                            chair_data[chair_number][0] = "D"
+                            chair_data[chair_number][1] = "0"
                             decrement_reserve()
                             id -= 1 
                         else:
                             print("ID informado não condiz com ID da poltrona!\n")
                             print("Somente o propietário pode cancelar a reserva")
 
-                    elif chair_data[chair_number][0] == 'C':
+                    elif chair_data[chair_number][0] == "C":
                         ID = int(input("Digite o id da passagem: "))
                         if str(ID) == chair_data[chair_number][1]:
                             print("Passagem cancelada")
-                            chair_data[chair_number][0] = 'D'
-                            chair_data[chair_number][1] = '0'
+                            chair_data[chair_number][0] = "D"
+                            chair_data[chair_number][1] = "0"
                             decrement_purchase()
                             id -= 1 
                         else:
                             print("ID informado não condiz com ID da poltrona!\n")
                             print("Somente o propietário pode cancelar a compra")
 
-                    elif chair_data[chair_number][0] == 'D':
+                    elif chair_data[chair_number][0] == "D":
                         print("Não é possível cancelar uma passagem que não tenha sido comprada / reservada")
                     else:
                         print("Poltrona não encontrada")
